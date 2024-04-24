3a5e0ecf41929a284bb30ec74a70d1d920ece824
Trevor Hutto <trevorhutto@fullstory.com>
2024-04-24 12:02:44 -0400

    fix: add docs around secret output

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -15,8 +15,14 @@ resource "google_bigquery_dataset" "main" {
   }
 }
 
-module "fullstory_data_integration" {
+module "fullstory_bigquery_setup" {
   source     = "fullstorydev/fullstory-bigquery-setup/google"
   dataset_id = google_bigquery_dataset.main.dataset_id
   project    = "my-project-id"
+}
+
+# This block is important so that the value can be viewed with `terraform output`.
+output "fullstory_bigquery_setup_service_account_private_key" {
+  value     = module.fullstory_bigquery_setup.service_account_private_key
+  sensitive = true
 }
